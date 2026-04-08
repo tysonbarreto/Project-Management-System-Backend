e15af60b95e65f6b6c4ac40c24f953ff0bcf2824
tyson <tysonbarretto1991@gmail.com>
2026-04-08 22:05:03 +0100

    cors configured

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}`);
+});
